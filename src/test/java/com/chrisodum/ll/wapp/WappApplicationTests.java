43c3982616f0e2216629ee9c847e138a3c2abe6e
Chris Odum <rcjo89@gmail.com>
2022-09-04 11:45:27 -0400

    started spring wapp project

---
+++ src/test/java/com/chrisodum/ll/wapp/WappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisodum.ll.wapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
